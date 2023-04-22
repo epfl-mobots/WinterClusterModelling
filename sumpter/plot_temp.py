87f66fec675320ba5fced061a8a61cbdcb445e52
bloues42 <48278430+lou-42@users.noreply.github.com>
2023-04-22 16:31:41 +0200

    tst

---
+++ sumpter/plot_temp.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import pickle
+import numpy as np
+
+def xU(grid):
+    exp = np.sum(grid[0])/49
+    xU = np.zeros((1,len(grid)))
+    for i in range(len(grid)):
+        n_y = np.sum(grid[i],axis=1)
+        xU[i]=np.sum((1/exp)*np.square(n_y-exp))
+    print(xU)
+    return xU
+
+path = "C:/Users/Louise/Documents/EPFL/MA4/Project/data/"
+
+# dir = ["2023-04-03T12_25_31/"]
+# plt.figure()
+# for d in dir:
+#     f = open(path+d+"beeGrid.obj", "rb")
+#     bg = pickle.load(f)
+#     f.close()
+#     print(len(bg))
+#     x = xU(bg)
+#     plt.plot(range(len(x)),x)
+#     plt.show()
+
+
+
+
+# dir = ["2023-04-03T15_36_48/","2023-04-03T15_52_29/","2023-04-03T16_04_40/","2023-04-03T16_17_13/","2023-04-03T16_35_44/"]
+# #evolution of Tmax over simulation time
+# for d in dir:
+#     f = open(path+d+"Tmax.obj", "rb")
+#     Tmax = pickle.load(f)
+#     f.close()
+#     plt.plot(range(len(Tmax)),Tmax)
+# plt.show()
+
+#bee distribution over time (along the x axis)
+d = "2023-04-17T11_34_50/"
+f = open(path+d+"beeGrid.obj", "rb")
+beegrid = pickle.load(f)
+f.close()
+
+beedistri = np.sum(beegrid,axis=1)
+print(np.shape(np.array(beegrid)))
+print(len(beedistri))
+
+for i in np.linspace(0,len(beedistri),10,dtype=int):
+  plt.figure()
+  plt.plot(range(len(beedistri[:,i])),beedistri[:,i])
+  plt.show()
+
---
+++ sumpter/test.py
@@ -0,0 +1,19 @@
+from matplotlib.colors import LinearSegmentedColormap, hsv_to_rgb
+import numpy as np
+import matplotlib.pyplot as plt
+import keyboard
+
+print(keyboard._os_keyboard.from_name[1])
+
+# colors=[]
+# for i in range(5):
+#     col = hsv_to_rgb((0+i*1/6,1,1))
+#     col = np.append(col,1.)
+#     colors.append(col)
+# cmap1 = LinearSegmentedColormap.from_list("mycmap", colors)
+
+# r = 1/10000*np.array(range(10000))
+# mat = np.reshape(r,(100,100))
+# print(mat)
+# plt.matshow(mat, cmap=cmap1,aspect='equal',interpolation='none',origin='lower')
+# plt.show()
